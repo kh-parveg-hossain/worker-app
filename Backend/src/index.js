e73c7d9fb99d2739d0f59a82a87ec7724c41c1a2
kh-parveg-hossain <parevgkh64@gmail.com>
2025-10-06 20:13:32 +0530

    d

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,21 +4,36 @@ import router from './routes/routes.js';
 import cors from 'cors';
 import { db } from './utils/db.js';
 import cookieParser from "cookie-parser";
+import cron from 'node-cron';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 db();
+
 app.use(cors({
-  origin: [ "https://worker-app-662a.vercel.app","https://worker-app-1.onrender.com","http://localhost:5173"], // your React frontend URLs
+  origin: [ "https://worker-app-662a.vercel.app","https://worker-app-1.onrender.com","http://localhost:5173"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true
 }));
 
- app.use('/', router);
 app.use(cookieParser());
+app.use('/', router);
 
+// ✅ API to hit every 14 minutes
+const targetAPI = 'https://worker-app-uoly.onrender.com';
 
+cron.schedule('*/14 * * * *', async () => {
+  try {
+    const res = await axios.get(targetAPI);
+    console.log('✅ Cron Job Hit API:', targetAPI, 'Response:', res.status);
+  } catch (err) {
+    console.error('❌ Cron Job Error:', err.message);
+  }
+});
+
+// Start server
 app.listen(config.port, () => {
   console.log(`Server is running on http://localhost:${config.port}`);
 });
